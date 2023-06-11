cd58e1b29b5339734717d1a0cdc34598303d4d5c
trollderius <trollderiu@gmail.com>
2023-06-11 16:49:33 +0200

    initial commit

--- app.py
+++ app.py
@@ -2,17 +2,19 @@ from potassium import Potassium, Request, Response
 
 from transformers import pipeline
 import torch
+import cv2
+import base64
 
 app = Potassium("my_app")
 
 # @app.init runs at startup, and loads models into the app's context
 @app.init
 def init():
-    device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
+    # device = 0 if torch.cuda.is_available() else -1
+    # model = pipeline('fill-mask', model='bert-base-uncased', device=device)
    
     context = {
-        "model": model
+        # "model": model
     }
 
     return context
@@ -20,12 +22,21 @@ def init():
 # @app.handler runs for every call
 @app.handler()
 def handler(context: dict, request: Request) -> Response:
-    prompt = request.json.get("prompt")
-    model = context.get("model")
-    outputs = model(prompt)
+    # prompt = request.json.get("prompt")
+    # model = context.get("model")
+    # outputs = model(prompt)
+
+    image_data = request.get('image_data', None)
+    image = Image.open(BytesIO(base64.b64decode(image_data))).convert("RGB") 
+
+    subprocess.run("./upscayl-realesrgan -i output-0.png -o output.jpeg -s 2 -m models -n ultramix_balanced -f jpeg")
+
+    img = cv2.imread('output.jpeg')
+    jpg_img = cv2.imencode('.jpeg', img)
+    b64_string = base64.b64encode(jpg_img[1]).decode('utf-8')
 
     return Response(
-        json = {"outputs": outputs[0]}, 
+        json = {"base64": b64_string}, 
         status=200
     )
 
